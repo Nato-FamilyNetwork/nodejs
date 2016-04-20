c2feddf99e135afed1b99df9c028d199ddecacb7
Zoolixx <mahdouch_clubiste@hotmail.fr>
2016-04-20 12:54:27 +0100

    test

--- routes/chat.js
+++ routes/chat.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
 var http = require('http');
 
@@ -52,4 +52,4 @@ for(var k in users){
 
 });
 
-module.exports = router;
+module.exports = router; */
