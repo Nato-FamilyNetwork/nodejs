7bc9bd0e2c9f2505190837ef1e9b38a26a8326f5
ch-bas <chagra.bassem@yahoo.fr>
2016-04-25 22:58:45 +0100

    Added files via upload

---
+++ model/hannibal.js
@@ -0,0 +1,14 @@
+var mongoose = require('../config/db');
+var Schema = mongoose.Schema;
+var schedualSchema = new Schema({
+    
+    
+    chaine: String,
+    program : {contenue: String, heur: String}
+	
+});
+
+module.exports = mongoose.model('hannibal',schedualSchema);
+
+
+
