0fb24410fb25d32e7eb4066c77fe6136c685e938
guesmiii <med.guesmi1992@gmail.com>
2016-04-27 10:32:02 +0200

    Update map.js

--- routes/map.js
+++ routes/map.js
@@ -3,13 +3,14 @@ var router = express.Router();
 var models = require('../model');
 
 
-router.get('/afficher', function(req, res, next) {
- var coor = models.comment.find({}).exec(function(err,coor){
-                       if(err) res.json({error:err});
-                    
-/*res.render('poly.twig',{coor:coor , user:req.user}); */ 
-     res.send(coor);
-           });
+router.get('/afficher/:id', function(req, res, next) {
+ var coor = models.comment.find({"userFK":req.params.id}, function(err, p){
+        if(err){
+            res.json({error: err});
+        }else{
+            res.json(p);
+        }
+    });
 });
 router.get('/affichermap', function(req, res, next) {
  var hi= 'hello';
@@ -19,7 +20,7 @@ res.render('map.twig',{hi:hi, user:req.user});
 
 router.post('/addmap', function(req, res, next) {
  
-  var c = new models.comment({attitude:req.body.mylat,longitude:req.body.mylong,date:req.body.date});
+  var c = new models.comment({attitude:req.body.mylat,longitude:req.body.mylong,date:req.body.date,userFK:req.body.userFK});
     c.save(function(err,c){
     if(err) req.json({error:err});
         res.json(c);
