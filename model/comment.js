016388b0fd106163b5ef199266a71cfc1b2bd705
guesmiii <med.guesmi1992@gmail.com>
2016-04-27 10:29:53 +0200

    Update comment.js

--- model/comment.js
+++ model/comment.js
@@ -3,5 +3,6 @@ var map = mongoose.Schema ({
 attitude: String,
     longitude: String,
     date: String,
+    userFK: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
 });
 module.exports = mongoose.model ( 'maps' , map );
