8a74c9eee8cb5336c4ef0b7d7938d9afb26706ec
Zoolixx <mahdouch_clubiste@hotmail.fr>
2016-04-29 23:07:11 +0100

    Updates to appear online/offline

--- routes/login.js
+++ routes/login.js
@@ -34,6 +34,27 @@ router.put('/update/:id/:league/:team', function(req, res, next) {
 });
 
 
+router.put('/online/:id/', function(req, res, next) {
+    Account.findByIdAndUpdate(req.params.id, {$set: {online:"true"}, {new: true}, function(err, category){
+        if(err){
+            res.send({error: err});
+        }else{
+            res.send(category);
+        }
+    });
+});
+
+router.put('/offline/:id/', function(req, res, next) {
+    Account.findByIdAndUpdate(req.params.id, {$set: {online:"false"}, {new: true}, function(err, category){
+        if(err){
+            res.send({error: err});
+        }else{
+            res.send(category);
+        }
+    });
+});
+
+
 
 //mahdi
 router.get('/all', function(req, res) {
