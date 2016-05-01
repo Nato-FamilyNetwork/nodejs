752209899dc3da6afcbd07c90bfb0833ef2260d4
DjiddouMoustapha <djiddoumahamat@gmail.com>
2016-05-01 17:38:49 +0100

    bassam

--- routes/foods.js
+++ routes/foods.js
@@ -1,3 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
+var models =require('../model');
+var fs=require('fs');
+  
+/* GET home page. */
+router.get('/', function(req, res, next) {
+     
+    
+     
+    models.food.find({}).exec(function(err,team){
+    if(err) res.send('Error');
+    res.send(team);
+    //res.render('pcs.twig', { title: 'List des pcs',tunisianetpcs:tunisianetpc, user:req.user });
+    
+});
+  
+    
+    
+      
+    
+  
+});
+
+router.post('/', function(req, res, next) {
+    var f = new models.food();
+    f.title = req.body.title;
+    f.contenue = req.body.contenue;
+   
+    //f.message.push(req.body.message[0]);
+    
+
+    //f.status = req.body.status;
+
+    f.save(function(err, todo){
+        if(err){
+            res.json({error: err});
+        }else{
+            res.json(todo);
+        }
+    });
+});
+router.put('/:id/:msg', function(req, res, next) {
+    var hh= {"nom":req.params.msg};
+     
+    
+    models.food.find({"title":{$regex: ".*"+req.params.id+".*", $options:"i"}}).exec(function(err,ts){
+     if(err) res.send('Error');
+    var idddddd=(ts[0]._id);
+         models.food.findById(idddddd, function(err, p){
+        if(err){
+            res.json({error: err});
+        }else{
+            
+            p.message.push(hh);
+            p.save();
+            res.json(p);
+            
+        }
+    });
+});
+            
+     
+});
 
 router.put('/update/:id/:id2/:note', function(req, res, next) {
    var hh=req.params.id2;
@@ -11,13 +77,14 @@ router.put('/update/:id/:id2/:note', function(req, res, next) {
             // look for the entry with a matching `code` value
             if (food[0].message[i]._id == req.params.id2){
               food[0].message[i].vote=req.params.note;
-              
+              //food.save();
+              res.send(food);
             }
           }
-            //food[0].save();
-              //res.send(food);
-            
+            res.send(food[0].message.length);
         }
     });
 });
   
+
+module.exports = router;
