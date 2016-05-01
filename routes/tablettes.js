d83416e0e83796247d9261981d40c563fa70046e
ch-bas <chagra.bassem@yahoo.fr>
2016-05-02 00:09:31 +0100

    Update tablettes.js

--- routes/tablettes.js
+++ routes/tablettes.js
@@ -47,7 +47,7 @@ request(url, function (error, response, body) {
       });
       
       
-      for(var ff=0;ff<images.length;ff++){
+      for(var ff=0;ff<imgs.length;ff++){
          var c = new models.tablette({
          	tablette:titles[i], 
          	lien:links[i],
