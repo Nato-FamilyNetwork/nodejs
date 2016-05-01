dfb694f032da8aef4bab29a1ce2e75a7c4891805
ch-bas <chagra.bassem@yahoo.fr>
2016-05-01 14:01:11 +0100

    Update foods.js

--- routes/foods.js
+++ routes/foods.js
@@ -66,8 +66,8 @@ router.put('/:id/:msg', function(req, res, next) {
 });
 
 router.put('/update/:id/:id2/:note', function(req, res, next) {
-   var hh=req.params.id2;
-    models.food.findByIdAndUpdate(req.params.id, {$set: {"message."+hh:{vote: req.params.note}}}, function(err, food){
+   var hh="message."+req.params.id2;
+    models.food.findByIdAndUpdate(req.params.id, {$set: {hh:{vote: req.params.note}}}, function(err, food){
         if(err){
             res.send({error: err});
         }else{
