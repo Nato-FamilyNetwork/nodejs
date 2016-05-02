ae191547e4e1ef7d0e8bb94c659e690265a9f88d
Zoolixx <mahdouch_clubiste@hotmail.fr>
2016-05-02 10:57:10 +0100

    Update pcs.js

--- routes/pcs.js
+++ routes/pcs.js
@@ -315,10 +315,10 @@ request(url, function (error, response, body) {
   } else {
     console.log("We’ve encountered an error: " + error);
   }
-    for(var i=0; i< techPC.length; i++){
-		var c = new models.pc({ram:ramArray[i],lien:arrayLien[i],photo:images[i], prix:arrayPrix[i],PC:techPC[i], source:"technopro",marque:marqueArray[i]});
-	c.save();
-	}
+ //   for(var i=0; i< techPC.length; i++){
+//		var c = new models.pc({ram:ramArray[i],lien:arrayLien[i],photo:images[i], prix:arrayPrix[i],PC:techPC[i], source:"technopro",marque:marqueArray[i]});
+//	c.save();
+//	}
  
     
 });
@@ -393,7 +393,7 @@ request(url, function (error, response, body) {
                cartGraphique=lawej(tnpc,"Carte graphique","-");
             
            var c = new models.pc({marque:marque,PC:pc,ram:ram,photo:images[i],prix:arrayPrix[i],lien:arrayLien[i],source:"tunisianet"});
-	c.save();
+//	c.save();
         });  
        console.log("ok tunisia net");
   } else {
@@ -484,7 +484,7 @@ request(urlMytek, function (error, response, body) {
           // console.log(arrayPrix[i]);
  
               
-	if(arrayPrix[j]!=undefined)c.save();
+	//if(arrayPrix[j]!=undefined)c.save();
            
         });  
         
