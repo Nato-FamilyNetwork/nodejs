cca69608679f1e29c024736f17fbbe42e4fc0f12
ch-bas <chagra.bassem@yahoo.fr>
2016-05-24 22:49:12 +0100

    Update pcs.js

--- routes/pcs.js
+++ routes/pcs.js
@@ -541,14 +541,16 @@ request(urlMytek, function (error, response, body) {
 
 //search by ressource
 router.get('/:search', function(req, res, next) {
-
-    
+ 
+    if( models.pc.find({$text:{$search:req.params.search}})){
                  models.pc.find({$text:{$search:req.params.search}},{score:{$meta:"textScore"}},{prix:1}).sort
         ({score:{$meta:"textScore"}}).exec(function(err,tunisianetpcs){
              if(err) res.send('Error');
             res.send(tunisianetpcs[0].prix+"on "+tunisianetpcs[0].source+" here is the link "+tunisianetpcs[0].lien );
         });
-        
+    }
+        else
+         res.send("no item found please enter a correct request");
      
   
  
