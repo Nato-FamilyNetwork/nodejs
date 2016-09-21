363f5c4ae91940c37bfe19e0b361ac73dda2594a
bassem chagra <chagra.bassem@yahoo.fr>
2016-09-21 21:35:17 +0100

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -1,5 +1,5 @@
 
-/*module.exports = function(io) {
+module.exports = function(io) {
 
  var express = require('express');
  var router = express.Router();
@@ -46,4 +46,4 @@ for(var k in users){
 
  return router;
 };
-*/
+
