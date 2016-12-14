e3183b296dc5d6bbbe7550f22b3d595972af94b6
bassem chagra <chagra.bassem@yahoo.fr>
2016-12-14 17:38:46 +0100

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -34,9 +34,9 @@ router.put('/update/:id/:league/:team', function(req, res, next) {
 });
 //radio
 router.put('/update/radio/:id/:radio', function(req, res, next) {
-    Account.findByIdAndUpdate(req.params.id, {$set: {radio:[]}}, function(err, category){
+    Account.findByIdAndUpdate(req.params.id, {$addToSet: {radio:req.params.radio}}, {new: true}, function(err, category){
         if(err){
-             Account.findByIdAndUpdate(req.params.id, {$addToSet: {radio:[req.params.radio]}});
+             res.send({error: err});
         }else{
             res.send(category);
         }
